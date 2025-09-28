7a75b3067804d564d291069cee46cba7d30f8a00
shreeshailmiragi18 <shreeshailbm18@gmail.com>
2025-09-28 13:20:24 +0530

    Complete About

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Tilt from 'react-parallax-tilt';
 import { TypeAnimation } from "react-type-animation";
+import ProfileImage from "../../assets/About/ProfilePicture.png";
 
 const About = () => {
   return (
@@ -33,10 +35,40 @@ const About = () => {
               style={{ fontSize: "semibold", display: "inline-block" }}
               repeat={Infinity}
             />
-                  </h3>
-                  <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-tight">
-                      Hi, I’m a university student passionate about web development and problem-solving. I enjoy learning new technologies and applying them in real projects. My focus is on building user-friendly, creative, and efficient applications as I grow my skills and prepare for future opportunities.
-                  </p>
+          </h3>
+          <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-tight">
+            Hi, I’m a university student passionate about web development and
+            problem-solving. I enjoy learning new technologies and applying them
+            in real projects. My focus is on building user-friendly, creative,
+            and efficient applications as I grow my skills and prepare for
+            future opportunities.
+          </p>
+
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105 "
+            style={{
+              background: "linear-gradient(90deg,#8245ec,#a855f7)",
+              boxShadow: "0 0 2px #8245ec, 0 0 40px #8245ec",
+            }}
+          >
+            Download Resume
+          </a>
+        </div>
+        <div className="md:w-1/2 flex justify-center md:justify-end">
+          <Tilt
+            tiltMaxAngleX={20}
+            tiltMaxAngleY={20}
+            perspective={1000}
+            scale={1.05}
+            transitionSpeed={1000}
+            gyroscope={true}
+            className="w-48 h-48 sm:w-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full"
+          >{/*className="w-48 h-48 sm:w-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full"*/}
+            <img src={ProfileImage} alt="Shreeshail M" className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"/>
+          </Tilt>
         </div>
       </div>
     </section>
