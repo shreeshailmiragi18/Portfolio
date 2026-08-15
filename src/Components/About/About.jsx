4d055dd58324b16442b983056cac8884df595c88
shreeshailmiragi18 <shreeshailbm18@gmail.com>
2026-08-15 19:39:21 +0530

    updated resume

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
 
           <a
-            href="https://drive.google.com/file/d/1Yb3MoaA37_JWJno04-_QtAe0DudhrLxC/view?usp=sharing"
+            href="https://drive.google.com/file/d/15Y25pu_sWxpcqhbygvJiA4V4hFzW2wQN/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105 "
